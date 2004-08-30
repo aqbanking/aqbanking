70bbca9b36ca8e9b8699772b04a1f752cf727eaf
aquamaniac <aquamaniac@5c42a225-8b10-0410-9873-89b7810ad06e>
2004-08-30 08:06:02 +0000

    added missing file
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@84 5c42a225-8b10-0410-9873-89b7810ad06e

---
+++ src/libs/aqbanking/types/accstatus_l.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ $RCSfile$
+                             -------------------
+    cvs         : $Id$
+    begin       : Mon Apr 05 2004
+    copyright   : (C) 2004 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *          Please see toplevel file COPYING for license details           *
+ ***************************************************************************/
+
+
+#ifndef AQBANKING_ACCSTATUS_L_H
+#define AQBANKING_ACCSTATUS_L_H
+
+#include <aqbanking/accstatus.h>
+#include <gwenhywfar/misc.h>
+
+GWEN_LIST_FUNCTION_LIB_DEFS(AB_ACCOUNT_STATUS,
+                            AB_AccountStatus,
+                            AQBANKING_API)
+
+
+#endif /* AQBANKING_ACCSTATUS_L_H */
+
+
