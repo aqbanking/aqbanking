dc7f9af6027cb6606b3dfbc4bba873d1ff979b02
aquamaniac <aquamaniac@5c42a225-8b10-0410-9873-89b7810ad06e>
2004-09-17 09:08:57 +0000

    added missing files :-}
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@120 5c42a225-8b10-0410-9873-89b7810ad06e

---
+++ src/plugins/imexporters/dtaus/dtaus_p.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ $RCSfile$
+                             -------------------
+    cvs         : $Id$
+    begin       : Mon Mar 01 2004
+    copyright   : (C) 2004 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *          Please see toplevel file COPYING for license details           *
+ ***************************************************************************/
+
+
+#ifndef AQHBCI_IMEX_DTAUS_P_H
+#define AQHBCI_IMEX_DTAUS_P_H
+
+
+#include <gwenhywfar/dbio.h>
+#include <aqbanking/imexporter_be.h>
+
+
+typedef struct AH_IMEXPORTER_DTAUS AH_IMEXPORTER_DTAUS;
+struct AH_IMEXPORTER_DTAUS {
+  GWEN_DB_NODE *dbData;
+  GWEN_DBIO *dbio;
+};
+
+
+AB_IMEXPORTER* dtaus_factory(AB_BANKING *ab, GWEN_DB_NODE *db);
+void AH_ImExporterDTAUS_FreeData(void *bp, void *p);
+
+int AH_ImExporterDTAUS_Import(AB_IMEXPORTER *ie,
+                              AB_IMEXPORTER_CONTEXT *ctx,
+                              GWEN_BUFFEREDIO *bio,
+                              GWEN_DB_NODE *params);
+
+int AH_ImExporterDTAUS_CheckFile(AB_IMEXPORTER *ie, const char *fname);
+
+int AH_ImExporterDTAUS__ImportFromGroup(AB_IMEXPORTER_CONTEXT *ctx,
+                                        GWEN_DB_NODE *db,
+                                        GWEN_DB_NODE *dbParams);
+
+#endif /* AQHBCI_IMEX_DTAUS_P_H */
---
+++ src/plugins/imexporters/swift/swift_p.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+ $RCSfile$
+                             -------------------
+    cvs         : $Id$
+    begin       : Mon Mar 01 2004
+    copyright   : (C) 2004 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *          Please see toplevel file COPYING for license details           *
+ ***************************************************************************/
+
+
+#ifndef AQHBCI_IMEX_SWIFT_P_H
+#define AQHBCI_IMEX_SWIFT_P_H
+
+
+#include <gwenhywfar/dbio.h>
+#include <aqbanking/imexporter_be.h>
+
+
+typedef struct AH_IMEXPORTER_SWIFT AH_IMEXPORTER_SWIFT;
+struct AH_IMEXPORTER_SWIFT {
+  GWEN_DB_NODE *dbData;
+  GWEN_DBIO *dbio;
+};
+
+
+AB_IMEXPORTER* swift_factory(AB_BANKING *ab, GWEN_DB_NODE *db);
+void AH_ImExporterSWIFT_FreeData(void *bp, void *p);
+
+int AH_ImExporterSWIFT_Import(AB_IMEXPORTER *ie,
+                              AB_IMEXPORTER_CONTEXT *ctx,
+                              GWEN_BUFFEREDIO *bio,
+                              GWEN_DB_NODE *params);
+
+int AH_ImExporterSWIFT_CheckFile(AB_IMEXPORTER *ie, const char *fname);
+
+
+int AH_ImExporterSWIFT__ImportFromGroup(AB_IMEXPORTER_CONTEXT *ctx,
+                                        GWEN_DB_NODE *db,
+                                        GWEN_DB_NODE *dbParams);
+
+#endif /* AQHBCI_IMEX_SWIFT_P_H */
