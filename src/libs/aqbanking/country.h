a9288f490bcb06064bc91a46f728a2b52309fa81
aquamaniac <aquamaniac@5c42a225-8b10-0410-9873-89b7810ad06e>
2005-03-22 14:46:35 +0000

    added APIDOC to AB_COUNTRY
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@246 5c42a225-8b10-0410-9873-89b7810ad06e

--- src/libs/aqbanking/country.h
+++ src/libs/aqbanking/country.h
@@ -28,27 +28,52 @@ GWEN_CONSTLIST2_FUNCTION_LIB_DEFS(AB_COUNTRY, AB_Country, AQBANKING_API)
 
 #include <aqbanking/banking.h>
 
-
+/**
+ * Returns the untranslated name of the given country.
+ */
 AQBANKING_API 
 const char *AB_Country_GetName(const AB_COUNTRY *cntry);
 
+/**
+ * Returns the ISO-3166-1 2-character code of the given country.
+ */
 AQBANKING_API 
 const char *AB_Country_GetCode(const AB_COUNTRY *cntry);
 
+/**
+ * Returns the ISO-3166-1 numeric code of the given country.
+ */
 AQBANKING_API 
 int AB_Country_GetNumericCode(const AB_COUNTRY *cntry);
 
+/**
+ * Returns a localized version of the name of the given country.
+ * If no localized version is available (i.e. because there is no translation)
+ * then the untranslated version is returned.
+ */
 AQBANKING_API 
 const char *AB_Country_GetLocalName(const AB_COUNTRY *cntry);
 
 
 
+/**
+ * Returns the ISO 4217 name of the currency used in the given
+ * country.
+ */
 AQBANKING_API 
 const char *AB_Country_GetCurrencyName(const AB_COUNTRY *cntry);
 
+/**
+ * Returns the 3-character ISO 4217 code of the currency used in the given
+ * country.
+ */
 AQBANKING_API 
 const char *AB_Country_GetCurrencyCode(const AB_COUNTRY *cntry);
 
+/**
+ * Returns a localized version of the ISO 4217 name of the currency used in
+ * the given country.
+ */
 AQBANKING_API 
 const char *AB_Country_GetLocalCurrencyName(const AB_COUNTRY *cntry);
 
