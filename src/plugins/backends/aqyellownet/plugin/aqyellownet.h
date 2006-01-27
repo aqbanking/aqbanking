1440b2a4c4f6d05b16b3bab6a11e0c3f25b5a919
aquamaniac <aquamaniac@5c42a225-8b10-0410-9873-89b7810ad06e>
2006-01-27 07:47:15 +0000

    minor fixes
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@867 5c42a225-8b10-0410-9873-89b7810ad06e

--- src/plugins/backends/aqyellownet/plugin/aqyellownet.h
+++ src/plugins/backends/aqyellownet/plugin/aqyellownet.h
@@ -52,24 +52,6 @@
 
 #define AQYELLOWNET_LOGDOMAIN "aqyellownet"
 
-#define AQYELLOWNET_FILEFLAGS_ESR_ASR        0x00000001
-#define AQYELLOWNET_FILEFLAGS_DEBIT_DIRECT   0x00000002
-#define AQYELLOWNET_FILEFLAGS_STATEMENTS     0x00000004
-#define AQYELLOWNET_FILEFLAGS_AVIS_CREDITOR  0x00000008
-#define AQYELLOWNET_FILEFLAGS_AVIS_DEBITOR   0x00000010
-#define AQYELLOWNET_FILEFLAGS_AVIS_DEPOT     0x00000020
-
-#define AY_URL_EINST_A \
-  "https://www.yellownet.ch/neu/onl_kdl_sess.sw_einst_a"
-#define AY_URL_EINST_B \
-  "https://www.yellownet.ch/neu/onl_kdl_sess.sw_einst_b"
-#define AY_URL_UPLOAD \
-  "https://www.yellownet.ch/cgi/upldsoftw_2"
-#define AY_URL_DOWNLOAD \
-  "https://www.yellownet.ch/cgi/sw_file_dwld"
-#define AY_URL_GETFILELIST \
-  "https://www.yellownet.ch/neu/onl_kdl_file.sw_file_list_get"
-
 
 #endif /* AQYELLOWNET_AQYELLOWNET_H */
 
