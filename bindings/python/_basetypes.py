1a19f65007b24f6473b5149ed0852dbec70c9aa9
christian <christian@5c42a225-8b10-0410-9873-89b7810ad06e>
2006-09-20 12:43:20 +0000

    Update so_version for python wrappers
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@1066 5c42a225-8b10-0410-9873-89b7810ad06e

--- bindings/python/_basetypes.py
+++ bindings/python/_basetypes.py
@@ -9,13 +9,13 @@ if sys.modules['ctypes'].__version__ < '0.9.5':
 
 
 if os.name == 'nt':
-    aqb = cdll.aqbanking32_14
+    aqb = cdll.aqbanking32_16
     gwen = cdll.gwenhywfar32_38
 elif os.name == 'mac':
     aqb = cdll.aqbanking
     gwen = cdll.gwenhywfar
 else:
-    aqb = cdll['libaqbanking.so.14']
+    aqb = cdll['libaqbanking.so.16']
     gwen = cdll['libgwenhywfar.so.38']
 
 
