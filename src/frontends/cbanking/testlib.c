53f81d157eb5bbb7b0d6cbf28b98bf2676555573
christian <christian@5c42a225-8b10-0410-9873-89b7810ad06e>
2007-02-07 16:47:08 +0000

    Add testcases for command line user feedback. Unfortunately, currently all of these do not work on windows :-(
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@1162 5c42a225-8b10-0410-9873-89b7810ad06e

--- src/frontends/cbanking/testlib.c
+++ src/frontends/cbanking/testlib.c
@@ -3,6 +3,35 @@
 int main(int argc, char *argv[])
 {
   AB_BANKING *testobject = CBanking_new("testlib", "testname");
+
+  if (argc > 1) {
+    char buffer[50];
+    int rv;
+
+    rv = AB_Banking_ShowBox(testobject, 0,
+			    "This is a ShowBox test title",
+			    "This is a ShowBox test.");
+    printf("AB_Banking_ShowBox: rv=%d\n", rv);
+    AB_Banking_HideBox(testobject, rv);
+    printf("AB_Banking_HideBox called.\n\n");
+
+    rv = AB_Banking_InputBox(testobject, 0,
+				 "This is a InputBox test title",
+				 "Just enter something.",
+				 buffer,
+				 1, 40);
+    printf("AB_Banking_InputBox: rv=%d, result=\"%s\"\n\n",
+	   rv, buffer);
+
+    rv = AB_Banking_MessageBox(testobject, 0,
+			       "Third test title, this time MessageBox",
+			       "Just press the first or second button.",
+			       "First button.", "Second button", NULL);
+    printf("AB_Banking_MessageBox: rv=%d; button=%s\n", rv,
+	   (rv == 1 ? "first" : (rv == 2 ? "second" : "unknown")));
+  } else
+    printf("For interaction test, run this:   ./testlib 1\n");
+
   AB_Banking_free(testobject);
 
   return 0;
