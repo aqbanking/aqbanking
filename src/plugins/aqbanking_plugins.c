3008f30650cfbf7af5e88a6c85c6cd5effcc40f7
martin <martin@5c42a225-8b10-0410-9873-89b7810ad06e>
2008-04-14 22:01:57 +0000

    - completed aqbanking_plugins.c
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@1474 5c42a225-8b10-0410-9873-89b7810ad06e

--- src/plugins/aqbanking_plugins.c
+++ src/plugins/aqbanking_plugins.c
@@ -33,6 +33,40 @@ GWEN_PLUGIN *provider_aqofxconnect_factory(GWEN_PLUGIN_MANAGER *pm,
 
 
 
+GWEN_PLUGIN *imexporters_csv_factory(GWEN_PLUGIN_MANAGER *pm,
+				     const char *name,
+				     const char *fileName);
+
+GWEN_PLUGIN *imexporters_dtaus_factory(GWEN_PLUGIN_MANAGER *pm,
+				       const char *name,
+				       const char *fileName);
+
+GWEN_PLUGIN *imexporters_eri2_factory(GWEN_PLUGIN_MANAGER *pm,
+				      const char *name,
+				      const char *fileName);
+
+GWEN_PLUGIN *imexporters_ofx_factory(GWEN_PLUGIN_MANAGER *pm,
+				     const char *name,
+				     const char *fileName);
+
+GWEN_PLUGIN *imexporters_openhbci1_factory(GWEN_PLUGIN_MANAGER *pm,
+					   const char *name,
+					   const char *fileName);
+
+GWEN_PLUGIN *imexporters_swift_factory(GWEN_PLUGIN_MANAGER *pm,
+				       const char *name,
+				       const char *fileName);
+
+GWEN_PLUGIN *imexporters_xmldb_factory(GWEN_PLUGIN_MANAGER *pm,
+				       const char *name,
+				       const char *fileName);
+
+GWEN_PLUGIN *imexporters_yellownet_factory(GWEN_PLUGIN_MANAGER *pm,
+					   const char *name,
+					   const char *fileName);
+
+
+
 GWEN_PLUGIN *bankinfo_at_factory(GWEN_PLUGIN_MANAGER *pm,
 				 const char *name,
 				 const char *fileName);
@@ -52,6 +86,14 @@ GWEN_PLUGIN *bankinfo_de_factory(GWEN_PLUGIN_MANAGER *pm,
 GWEN_PLUGIN *bankinfo_us_factory(GWEN_PLUGIN_MANAGER *pm,
 				 const char *name,
 				 const char *fileName);
+
+GWEN_PLUGIN *dbio_dtaus_factory(GWEN_PLUGIN_MANAGER *pm,
+				const char *modName,
+				const char *fileName);
+
+GWEN_PLUGIN *dbio_swift_factory(GWEN_PLUGIN_MANAGER *pm,
+				const char *modName,
+				const char *fileName);
 
 
 
@@ -84,20 +126,44 @@ int AB_Plugins_Init() {
   pm=GWEN_PluginManager_FindPluginManager("imexporters");
   if (pm) {
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_CSV
+    p=imexporters_csv_factory(pm, "csv", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_DTAUS
+    p=imexporters_dtaus_factory(pm, "dtaus", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_ERI2
+    p=imexporters_eri2_factory(pm, "eri2", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_OFX
+    p=imexporters_ofx_factory(pm, "ofx", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_OPENHBCI1
+    p=imexporters_openhbci1_factory(pm, "openhbci1", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_SWIFT
+    p=imexporters_swift_factory(pm, "swift", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_XMLDB
+    p=imexporters_xmldb_factory(pm, "xmldb", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_IMEXPORTER_YELLOWNET
+    p=imexporters_yellownet_factory(pm, "yellownet", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
   }
 
@@ -105,14 +171,29 @@ int AB_Plugins_Init() {
   pm=GWEN_PluginManager_FindPluginManager("bankinfo");
   if (pm) {
 #ifdef AQBANKING_WITH_PLUGIN_BANKINFO_AT
+    p=bankinfo_at_factory(pm, "at", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_BANKINFO_CA
+    p=bankinfo_ca_factory(pm, "ca", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_BANKINFO_CH
+    p=bankinfo_ch_factory(pm, "ch", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_BANKINFO_DE
+    p=bankinfo_de_factory(pm, "de", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_BANKINFO_US
+    p=bankinfo_us_factory(pm, "us", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
   }
 
@@ -120,8 +201,14 @@ int AB_Plugins_Init() {
   pm=GWEN_PluginManager_FindPluginManager("dbio");
   if (pm) {
 #ifdef AQBANKING_WITH_PLUGIN_PARSERS_DTAUS
+    p=dbio_dtaus_factory(pm, "dtaus", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
 #ifdef AQBANKING_WITH_PLUGIN_PARSERS_SWIFT
+    p=dbio_swift_factory(pm, "swift", NULL);
+    if (p)
+      GWEN_PluginManager_AddPlugin(pm, p);
 #endif
   }
 
