7fd953c5ae45d6d05f983117f1b93bdba6aed5be
martin <martin@5c42a225-8b10-0410-9873-89b7810ad06e>
2012-01-01 11:38:45 +0000

    Added a hint to a possible solution to an error message.
    git-svn-id: https://devel.aqbanking.de/svn/aqbanking/trunk@2276 5c42a225-8b10-0410-9873-89b7810ad06e

--- src/plugins/backends/aqhbci/plugin/accountjobs/accountjob.c
+++ src/plugins/backends/aqhbci/plugin/accountjobs/accountjob.c
@@ -83,7 +83,9 @@ AH_JOB *AH_AccountJob_new(const char *name,
       GWEN_Gui_ProgressLog(0,
 			   GWEN_LoggerLevel_Error,
 			   I18N("This job needs an account suffix, but your bank didn't provide one. "
-				"Therefore this job is not supported with your account."));
+                                "Therefore this job is not supported with your account.\n"
+                                "Setting a higher HBCI version in the user settings might fix "
+                                "the problem."));
       return NULL;
     }
     if (maxVer>0) {
