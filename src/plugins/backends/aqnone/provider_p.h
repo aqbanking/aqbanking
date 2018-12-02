02beb882d2b6a59efb0c417826c399313933e7a6
Martin Preuss <martin@aquamaniac.de>
2018-12-02 01:55:00 +0100

    AqNone: Adapted to AqBanking6. Well... not much to be done...

--- src/plugins/backends/aqnone/provider_p.h
+++ src/plugins/backends/aqnone/provider_p.h
@@ -1,6 +1,6 @@
 /***************************************************************************
     begin       : Mon Mar 01 2004
-    copyright   : (C) 2004-2010 by Martin Preuss
+    copyright   : (C) 2018 by Martin Preuss
     email       : martin@libchipcard.de
 
  ***************************************************************************
@@ -14,17 +14,6 @@
 
 int AN_Provider_Init(AB_PROVIDER *pro, GWEN_DB_NODE *dbData);
 int AN_Provider_Fini(AB_PROVIDER *pro, GWEN_DB_NODE *dbData);
-int AN_Provider_UpdateJob(AB_PROVIDER *pro, AB_JOB *j);
-int AN_Provider_AddJob(AB_PROVIDER *pro, AB_JOB *j);
-int AN_Provider_ResetQueue(AB_PROVIDER *pro);
-int AN_Provider_Execute(AB_PROVIDER *pro,
-			AB_IMEXPORTER_CONTEXT *ctx);
-int AN_Provider_ExtendUser(AB_PROVIDER *pro, AB_USER *u,
-			   AB_PROVIDER_EXTEND_MODE em,
-			   GWEN_DB_NODE *dbBackend);
-int AN_Provider_ExtendAccount(AB_PROVIDER *pro, AB_ACCOUNT *a,
-			      AB_PROVIDER_EXTEND_MODE em,
-			      GWEN_DB_NODE *dbBackend);
 
 #endif
 
