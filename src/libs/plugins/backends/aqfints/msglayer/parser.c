0df386540bcdf270117d59843c0023a89dff77ed
Martin Preuss <martin@aquamaniac.de>
2019-06-24 23:15:06 +0200

    Experimented with new parser for fints.

---
+++ src/libs/plugins/backends/aqfints/msglayer/parser.c
@@ -0,0 +1,28 @@
+/***************************************************************************
+ begin       : Sun Jun 23 2019
+ copyright   : (C) 2019 by Martin Preuss
+ email       : martin@libchipcard.de
+
+ ***************************************************************************
+ * This file is part of the project "AqBanking".                           *
+ * Please see toplevel file COPYING of that project for license details.   *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+
+/*
+
+  TODO:
+  - ReadXml
+  - WriteXml
+
+  - ReadFints
+  - WriteFints
+
+  - SetupFromDb
+
+ */
+
