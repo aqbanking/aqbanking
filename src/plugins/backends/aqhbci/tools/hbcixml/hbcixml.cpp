ba4a209baba5091846cd20bef1d3e9682c53d3f5
Martin Preuss <martin@aqbanking.de>
2019-08-07 15:12:55 +0200

    AqHBCI: Fixed a compiler warning.

--- src/plugins/backends/aqhbci/tools/hbcixml/hbcixml.cpp
+++ src/plugins/backends/aqhbci/tools/hbcixml/hbcixml.cpp
@@ -64,7 +64,7 @@ using namespace std;
 
 #define MYNAME "hbcixml2"
 #define PRG_VERSION_INFO \
-  MYNAME " v1.99  (part of AqHBCI v"AQHBCI_VERSION_STRING")\n"\
+  MYNAME " v1.99  (part of AqHBCI v" AQHBCI_VERSION_STRING ")\n"\
   "(c) 2005 Martin Preuss<martin@libchipcard.de>\n" \
   "This program is free software licensed under GPL.\n"\
   "See COPYING for details.\n"
