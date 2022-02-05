5eaf208d9573d975c8672ec8cd7d954641a85e26
Martin Preuss <martin@aquamaniac.de>
2022-02-05 18:19:03 +0100

    AqBanking: Improved backend support.
    - provide fallback for AB_Provider_CreateUserObject
    - provide fallback for AB_Provider_CreateAccountObject
    - slightly improved documentation

--- src/libs/aqbanking/backendsupport/provider.h
+++ src/libs/aqbanking/backendsupport/provider.h
@@ -56,52 +56,9 @@ extern "C" {
  *
  * @brief This group represents backends. (Don't use in applications)
  *
- * <p>
  * (<i>Provider</i> is simply another word for <i>backend</i>.)
- * </p>
  *
- * <p>
- * Functions in this group <b>MUST NEVER</b> be used by applications or
- * depending libraries ! They may only be called by AqBanking or a provider
- * on its own.
- * </p>
- *
- * <p>
- * Writing an online banking provider for AqBanking is easy. There are only
- * a few callback functions which must be set by the provider (marked as
- * <i>Virtual Functions</i> below).
- * </p>
- *
- * <p>
- * The work of a provider is based on jobs (see @ref AB_JOB).
- * AqBanking also works based on jobs. If the application wants to create
- * a job AqBanking calls the function @ref AB_Provider_UpdateJob. This
- * function lets the provider prepare some parameters for the job given (e.g.
- * the maximum number of purpose lines for transfer jobs etc). These limits
- * are used by applications when preparing a job.
- * </p>
- * <p>
- * If the application is finished preparing the job it calls
- * @ref AB_Banking_EnqueueJob. After the application has enqueued all jobs
- * it calls @ref AB_Banking_ExecuteQueue. This function now sends all jobs
- * to their respective providers using @ref AB_Provider_AddJob. When all
- * jobs for a given provider are added AqBanking calls
- * @ref AB_Provider_Execute on this provider. This functions really sends the
- * jobs to the bank server or creates DTAUS discs or whatever the provider is
- * supposed to do.
- * After that AqBanking calls @ref AB_Provider_ResetQueue to make sure no job
- * is left in the providers queue after execution.
- * </p>
- * <p>
- * Another base class used between AqBanking and providers is @ref AB_ACCOUNT.
- * An account stores a reference to its associated provider.
- * When executing @ref AB_Banking_Init AqBanking calls the provider function
- * @ref AB_Provider_ExtendAccount on every account to let the backend
- * initialize the account.
- * </p>
- * <p>
- * It is the same with @ref AB_USER.
- * </p>
+ * See https://www.aquamaniac.de/rdm/projects/aqbanking/wiki/Develop_backend
  */
 /*@{*/
 
