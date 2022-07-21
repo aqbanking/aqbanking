d3f1ec8381680621e3a6405b47afa96b2466f1c9
Martin Preuss <martin@aquamaniac.de>
2022-07-21 21:30:13 +0200

    banking: Added example on how to use import dialogs-

--- src/libs/aqbanking/banking_dialogs.h
+++ src/libs/aqbanking/banking_dialogs.h
@@ -20,13 +20,49 @@
 extern "C" {
 #endif
 
+/** @name Import Dialogs
+ *
+ * Dialogs from this group can be used to import any file type AqBanking supports.
+ * Those dialogs are easy to use:
+ *
+ * @code{.c}
+    AB_IMEXPORTER_CONTEXT *importAnyFileIntoContext(AB_BANKING *ab) {
+      GWEN_DIALOG *dlg;
+      AB_IMEXPORTER_CONTEXT *ctx;
+      int rv;
+  
+      ctx=AB_ImExporterContext_new();
+      dlg=AB_Banking_CreateImporterDialog(ab, ctx, "This is a simple finish message.");
+      if (dbg=NULL) {
+	fprintf(stderr, "Could not create dialog\n");
+	AB_ImExporterContext_free(ctx);
+	return NULL;
+      }
+      GWEN_Dialog_SetWidgetText(dlg, "", "My Selected Window Title");
+      rv=GWEN_Gui_ExecDialog(dlg, 0);
+      if (rv==0) {
+	fprintf(stderr, "Rejected.\n");
+	GWEN_Dialog_free(dlg);
+	AB_ImExporterContext_free(ctx);
+	return NULL;
+      }
+  
+      GWEN_Dialog_free(dlg);
+      return ctx;
+   }
+
+ * @endcode
+ */
+/*@{*/
+
+
 /**
  * Creates a file import assistent dialog.
  *
  * @return pointer to the created dialog.
  * @param banking pointer to the AqBanking object
  * @param ctx pointer to the import context to receive the content of the
- *   imported file
+ *   imported file (not taken over, the caller remains owner of the object)
  * @param finishedMessage message to show on the last page of the assistent
  *   (i.e. the page which is shown after a successfull import into the given
  *    import context).
@@ -68,6 +104,10 @@ GWEN_DIALOG *AB_Banking_CreateImporterDialogWithPresets(AB_BANKING *ab,
  */
 AQBANKING_API
 void AB_Banking_ImporterDialogGatherPresets(GWEN_DIALOG *dlg, GWEN_DB_NODE *dbPrefs);
+
+
+/*@}*/
+
 
 
 AQBANKING_API
