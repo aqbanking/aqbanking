a29bbdeca561c03df822fdfe78cf6fb870824292
Martin Preuss <martin@aquamaniac.de>
2022-07-30 18:19:05 +0200

    SWIFT: Use shared code in parser for 940/60.

--- src/libs/plugins/parsers/swift/swift940_60.c
+++ src/libs/plugins/parsers/swift/swift940_60.c
@@ -13,45 +13,24 @@
 
 #include "swift940_60.h"
 
-
 #include <aqbanking/error.h>
-#include <aqbanking/backendsupport/imexporter_be.h>
 
-#include <gwenhywfar/text.h>
 #include <gwenhywfar/debug.h>
-#include <gwenhywfar/gwentime.h>
 #include <gwenhywfar/gui.h>
 
 #include <ctype.h>
 
 
 
-/* ------------------------------------------------------------------------------------------------
- * forward declarations
- * ------------------------------------------------------------------------------------------------
- */
-
-
-
-
-/* ------------------------------------------------------------------------------------------------
- * implementations
- * ------------------------------------------------------------------------------------------------
- */
-
-int AHB_SWIFT940_Parse_60_62(const AHB_SWIFT_TAG *tg,
-                             uint32_t flags,
-                             GWEN_DB_NODE *data,
-                             GWEN_DB_NODE *cfg)
+int AHB_SWIFT940_Parse_60_62(const AHB_SWIFT_TAG *tg, uint32_t flags, GWEN_DB_NODE *data, GWEN_DB_NODE *cfg)
 {
   const char *p;
   const char *p2;
   char *s;
   char buffer[32];
   unsigned int bleft;
-  int d1, d2, d3;
   int neg;
-  GWEN_DATE *dt;
+  GWEN_DATE *balanceDate;
 
   p=AHB_SWIFT_Tag_GetData(tg);
   assert(p);
@@ -60,9 +39,8 @@ int AHB_SWIFT940_Parse_60_62(const AHB_SWIFT_TAG *tg,
   /* credit/debit mark (M) */
   if (bleft<2) {
     DBG_ERROR(AQBANKING_LOGDOMAIN, "Bad value string (%s)", p);
-    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error,
-                         "SWIFT: Bad value string");
-    return -1;
+    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error, "SWIFT: Bad value string");
+    return GWEN_ERROR_GENERIC;
   }
   neg=0;
   if (*p=='D' || *p=='d')
@@ -71,37 +49,23 @@ int AHB_SWIFT940_Parse_60_62(const AHB_SWIFT_TAG *tg,
   bleft--;
 
   /* date (M) */
-  if (bleft<6) {
+  balanceDate=AHB_SWIFT_ReadDateYYMMDD(&p, &bleft);
+  if (balanceDate==NULL) {
     DBG_ERROR(AQBANKING_LOGDOMAIN, "Missing date (%s)", p);
-    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error,
-                         "SWIFT: Missing date");
-    return -1;
+    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error, "SWIFT: Missing date");
+    return GWEN_ERROR_GENERIC;
   }
-  d1=((p[0]-'0')*10) + (p[1]-'0');
-  if (d1>AHB_SWIFT_CENTURY_CUTOFF_YEAR)
-    d1+=1900;
-  else
-    d1+=2000;
-  d2=((p[2]-'0')*10) + (p[3]-'0');
-  d3=((p[4]-'0')*10) + (p[5]-'0');
+  GWEN_DB_SetCharValue(data, GWEN_DB_FLAGS_OVERWRITE_VARS, "date", GWEN_Date_GetString(balanceDate));
+  GWEN_Date_free(balanceDate);
 
-  dt=GWEN_Date_fromGregorian(d1, d2, d3);
-  assert(dt);
-  GWEN_DB_SetCharValue(data, GWEN_DB_FLAGS_OVERWRITE_VARS, "date", GWEN_Date_GetString(dt));
-  GWEN_Date_free(dt);
-
-  p+=6;
-  bleft-=6;
 
   /* currency (M) */
   if (!isdigit(*p)) {
-    /* only read currency if this is not part of the value (like in some
-     * swiss MT940) */
+    /* only read currency if this is not part of the value (like in some swiss MT940) */
     if (bleft<3) {
       DBG_ERROR(AQBANKING_LOGDOMAIN, "Missing currency (%s)", p);
-      GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error,
-                           "SWIFT: Missing currency");
-      return -1;
+      GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error, "SWIFT: Missing currency");
+      return GWEN_ERROR_GENERIC;
     }
     memmove(buffer, p, 3);
     buffer[3]=0;
@@ -113,9 +77,8 @@ int AHB_SWIFT940_Parse_60_62(const AHB_SWIFT_TAG *tg,
   /* value (M) */
   if (bleft<1) {
     DBG_ERROR(AQBANKING_LOGDOMAIN, "Missing value (%s)", p);
-    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error,
-                         "SWIFT: Missing value");
-    return -1;
+    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error, "SWIFT: Missing value");
+    return GWEN_ERROR_GENERIC;
   }
 
   p2=p;
@@ -123,9 +86,8 @@ int AHB_SWIFT940_Parse_60_62(const AHB_SWIFT_TAG *tg,
     p2++;
   if (p2==p) {
     DBG_ERROR(AQBANKING_LOGDOMAIN, "Bad value (%s)", p);
-    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error,
-                         "SWIFT: Bad value");
-    return -1;
+    GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error, "SWIFT: Bad value");
+    return GWEN_ERROR_GENERIC;
   }
   s=(char *)GWEN_Memory_malloc(p2-p+1+(neg?1:0));
   if (neg) {
